3c6c661e39dac05d20bffa8df6b0a8572a4078e0
Alexey Karpechenko <karpechenko.lesha@gmail.com>
2020-01-17 11:50:48 +0300

    Minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cinemas service / Karpechenko Alexey / 171-331')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('cinema')
+    .addTag('films')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
